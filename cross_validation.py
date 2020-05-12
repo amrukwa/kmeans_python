6f00f408b5d7e358850337d4ef6df8124229ed76
amrukwa <a.mrukwa00@gmail.com>
2020-05-12 09:14:31 +0200

    Add score

--- cross_validation.py
+++ cross_validation.py
@@ -7,15 +7,17 @@ class KFold:
         self.n_splits = n_splits
 
     def validate(self, data, labels_true, estimator):
+        score = 0
         indices = [i for i in range(data.shape[0])]
         np.random.shuffle(indices)
         split = np.array_split(indices, self.n_splits)
-
         for array in split:
-            test = np.array([data[i] for i in range(array.shape[0])])
-            train = np.array([data[i] for i in range(array.shape[0], data.shape[0])])
-
-        score = 0
+            test = np.array([data[array[i]] for i in range(array.shape[0])])
+            test_true = np.array([labels_true[array[i]] for i in range(array.shape[0])])
+            train = np.delete(data, array, axis=0)
+            estimator.fit(train)
+            labels_pred = estimator.predict(test)
+            score += metrics.adjusted_rand_score(test_true, labels_pred)
         return score / self.n_splits
 
 
