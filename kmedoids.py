76379ee799017ca5d707a60a1ad3351e137bb468
amrukwa <a.mrukwa00@gmail.com>
2020-05-14 07:50:35 +0200

    Implement k-medoids: draft

---
+++ kmedoids.py
@@ -0,0 +1,64 @@
+import kmeans as km
+import kmeans_functions as kmf
+import sklearn.utils as utils
+import scipy.spatial.distance as ssdist
+import numpy as np
+
+
+class KMedoids(km.KMeans):
+    def fit(self, x):
+        self.distance = ssdist.cdist(x, x, self.metric)
+        self.n_iter_ = 0
+        self.random_state = utils.check_random_state(self.random_state)
+        self.centroids_indices_ = initialization(self.n_clusters, self.initialize, x, self.random_state, self.metric)
+        self.labels_ = labeling(self.centroids_indices_, x, self.distance)
+
+        self.centroids = x[self.centroids_indices_]
+        return self
+
+    def predict(self, matrix):
+        labels = kmf.labeling(self.centroids, matrix, self.metric)
+        return labels
+
+
+def initialization(n_clusters, initialize, x, random_state, metric):
+    # this method initializes the first set of labels
+    if initialize == 'random':
+        centroids_indices = random_init(x, n_clusters, random_state)
+    elif initialize == 'k-means++':
+        centroids_indices = kpp_init(x, n_clusters, metric)
+    return centroids_indices
+
+
+def random_init(data, n_clusters, random_state):
+    np.random.seed(random_state)
+    indices = np.random.choice(data.shape[0], n_clusters, replace=False)
+    return indices
+
+
+def next_for_kpp(centroids_indices, x, metric):
+    datasize = x.shape[0]
+    dist = ssdist.cdist(x, x[centroids_indices], metric)  # change for better performance
+    p_dist = dist.min(axis=1)
+    p_dist = p_dist**2
+    all_dist = np.sum(p_dist)
+    probability = [(p_dist[i]) / all_dist for i in range(datasize)]
+    new_index = np.random.choice(datasize, 1, p=probability)
+    return new_index
+
+
+def kpp_init(data, n_clusters, metric):
+    datasize = data.shape[0]
+    centroid_indices = np.array(np.random.choice(datasize, 1))
+    k = 1
+    while k < n_clusters:
+        new_centre = next_for_kpp(centroid_indices, data, metric)
+        centroids_indices = np.append(centroid_indices, new_centre, axis=0)
+        k += 1
+    return centroids_indices
+
+
+def labeling(centroids_indices, x, distance):
+    dist_local = distance[:, centroids_indices]
+    labels = dist_local.argmin(axis=1)
+    return labels
--- run_file.py
+++ run_file.py
@@ -6,21 +6,24 @@ import sklearn.preprocessing as pre
 import cross_validation as cv
 import umap
 import plotly.express as px
-import sklearn.metrics as metrics
-
+import numpy as np
+import kmedoids as kmd
 
 if __name__ == "__main__":
     iris = pd.read_csv("iris.data", header=None)
     labels_true = iris[4]
     data = iris.drop(columns=4).values
+    shape = data.shape
+    essence, counts = np.unique(data, axis=0, return_counts=True)
     # X, y = sd.make_blobs()
     transformer = pre.StandardScaler()
     data = transformer.fit_transform(data)
-    est = km.KMeans(2)
-    est = est.fit(data)
-    dunn = cn.Dunn(est)
-    est = dunn.fit(data)
-    print(est.n_clusters)
-    val = cv.KFold()
-    score1 = val.validate(data, labels_true, est)
-    print(score1)
+    # est = km.KMeans(2)
+    # est = est.fit(data)
+    # dunn = cn.Dunn(est)
+    # est = dunn.fit(data)
+    # print(est.n_clusters)
+    # val = cv.KFold()
+    # score1 = val.validate(data, labels_true, est)
+    # print(score1)
+    est = kmd.KMedoids
